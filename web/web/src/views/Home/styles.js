167a6b37de7cd34eb2d2f48e0edcfcb5d2f1b4e4
unknown <caioviniciusg.1000@gmail.com>
2020-12-28 01:19:05 -0300

    Criando os cartões de tarefas

--- web/web/src/views/Home/styles.js
+++ web/web/src/views/Home/styles.js
@@ -4,3 +4,19 @@ export const Container = styled.div`
 
     width: 100%;
 `
+
+export const FilterArea = styled.div`
+    
+    width: 100%;
+    display: flex;
+    /* pesquisar sobre dps, e sobre responsividade
+    flex-wrap: wrap;
+    */
+    justify-content: space-around;
+    margin-top: 30px;
+
+    button{
+        background: none;
+        border: none;
+    }
+`
