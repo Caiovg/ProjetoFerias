0a2fee1fb061482755cc4f4f072bfc801095ea74
unknown <caioviniciusg.1000@gmail.com>
2020-12-28 20:37:01 -0300

    Conectando o Frontend com o Backend e listando as tarefas

--- web/web/src/components/TaskCard/index.js
+++ web/web/src/components/TaskCard/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, {useMemo} from 'react';
+import { format } from 'date-fns';
 import * as S from './styles';
 
 import iconDefault from '../../assets/default.png';
 
-function TaskCard() {
-    return (
-      <S.Container>
-        <S.TopCard>
-            <img src={iconDefault} alt="Icone da Tarefa"/>
-            <h3>Título da Tarefa</h3>
-        </S.TopCard>
-        <S.BottomCard>
-            <strong>17/10/2020</strong>
-            <span>10:00</span>
-        </S.BottomCard>
-      </S.Container>
-    )
+function TaskCard({type, title, when}) {
+  /**para converter a hora */
+  const date = useMemo(() => format(new Date(when), 'dd/MM/yyyy') );
+  const hour = useMemo(() => format(new Date(when), 'HH:mm') );
+
+  return (
+    <S.Container>
+      <S.TopCard>
+          <img src={iconDefault} alt="Icone da Tarefa"/>
+          <h3>{title}</h3>
+      </S.TopCard>
+      <S.BottomCard>
+          <strong>{date}</strong>
+          <span>{hour}</span>
+      </S.BottomCard>
+    </S.Container>
+  )
 }
   
 export default TaskCard;
--- web/web/src/views/Home/index.js
+++ web/web/src/views/Home/index.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './styles';
+
+import api from '../../services/api';
+
 //nossos componentes
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
@@ -8,6 +11,42 @@ import TaskCard from '../../components/TaskCard'
 
 function Home() {
   const [filterActived, setFilterActived] = useState('all');
+
+  /**Uma variavel para guardar temporariamente todas as informações que veio da api */
+  const[tasks, setTasks] = useState([]);
+
+  /**Carregar os dados da api */
+  async function loadTasks(){
+    await api.get(`/task/filter/${filterActived}/11:11:11:11:11:11`)
+    .then(response =>{
+      setTasks(response.data)
+      console.log(response.data)
+    })
+  }
+  
+  /** */
+  /**ATENÇÂO: Caso não apareça nada do back para p front, muito possivelmente vc esqueceu de configurar o CORS
+   * CORS e o que permite que as aplicações se conversem, então volte la no backend e configure a pasta
+   * primeira coisa na pasta backend abra o terminal e instale o cors
+   * 'npm install cors'
+   * Agr vá ate o index do backend e crie uma variavel lá do cors
+   * 'const cors = require('cors');'
+   * agr utilize ele
+   * 'server.use(cors());'
+  */
+  /** */
+
+  /**E uma função do react para que toda vez que a tela recarregar ele faça alguma coisa */
+  /**nesse caso o react quando recarregar a pagina
+   * vai chamar o loadTasks para que busque as informações na api
+   * e quando o estado do filtro muda o filterActived ele vai recarregar a pagina 
+   * so que com outras informaçoes do banco dependendo do filtro que foi escolhido
+   */
+  useEffect(() =>{
+    loadTasks();
+  }, [filterActived])
+
+
   return (
     <S.Container>
         <Header/>
@@ -34,19 +73,13 @@ function Home() {
           </S.Title>
 
           <S.Content>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
+            {
+              /**o map ele vai percorrer item por item */
+              tasks.map(t => (
+                <TaskCard type={t.type} title={t.title} when={t.when} />
+              ))
+            }
           </S.Content>  
-        
         <Footer/>
     </S.Container>
   )
